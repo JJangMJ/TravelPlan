45ad089dab335d77bb5741257a78775eedc3bc2a
JMJ <ghtntkdnsk@naver.com>
2023-12-18 03:29:21 +0900

    Study

--- app/src/main/java/com/example/travelplan/MainActivity.kt
+++ app/src/main/java/com/example/travelplan/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.travelplan
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.travelplan.databinding.ActivityMainBinding
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,16 +17,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.addPlanBtn.setOnClickListener {
-            val intent = Intent(this, AddPlanActivity::class.java)
+            val intent = Intent(this, SearchDestinationActivity::class.java)
             startActivity(intent)
         }
 
-        var planList = ArrayList<Plan>()
-        planList.add(Plan("a", "일본", LocalDate.now(), LocalDate.now()))
-        planList.add(Plan("b", "중국", LocalDate.now(), LocalDate.now()))
-        planList.add(Plan("c", "대만", LocalDate.now(), LocalDate.now()))
+        val planList = ArrayList<Plan>()
+        planList.add(Plan("a", "일본", LocalDate.now(), LocalDate.now().plusDays(10)))
+        planList.add(Plan("b", "중국", LocalDate.now(), LocalDate.now().plusDays(5)))
+        planList.add(Plan("c", "대만", LocalDate.now(), LocalDate.now().plusDays(4)))
 
-        var adapter = PlanAdapter(planList)
+        val adapter = PlanAdapter(planList)
         adapter.planList = planList
 
         binding.planListRv.adapter = adapter
--- app/src/main/java/com/example/travelplan/PlanActivity.kt
+++ app/src/main/java/com/example/travelplan/PlanActivity.kt
@@ -3,13 +3,15 @@ package com.example.travelplan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.travelplan.databinding.ActivityPlanBinding
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 class PlanActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPlanBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityPlanBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -17,6 +19,19 @@ class PlanActivity : AppCompatActivity() {
         binding.destinationTv.setText(plan.destination)
         binding.departureDateTv.setText(plan.startDate.toString())
         binding.endDateTv.setText(plan.endDate.toString())
-        Log.d("$$$", plan!!.toString())
+
+        val dayGap = ChronoUnit.DAYS.between(plan.startDate, plan.endDate)
+        val dayList = ArrayList<LocalDate>()
+
+        for (i in 0..dayGap) {
+            dayList.add(plan.startDate.plusDays(i))
+        }
+
+        // dayList -> Recycler
+        val adapter = DayAdapter(dayList)
+
+        binding.dayRv.adapter = adapter
+        binding.dayRv.layoutManager = LinearLayoutManager(this)
+
     }
 }
